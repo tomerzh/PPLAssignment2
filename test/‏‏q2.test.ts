1e40866a9ca624a40158f46e9cce866238af8e9b
tomerzh <tomerzkatom7@gmail.com>
2022-04-20 12:51:32 +0300

    get dict done

--- test/‏‏q2.test.ts
+++ test/‏‏q2.test.ts
@@ -89,13 +89,13 @@ describe('Q4 Tests', () => {
      */
     it("Q22", () => {
         expect(evalP(`(L3` + q2 + q2_test_string + `3)`)).to.deep.equal(makeOk(3));
-        //expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (dict? dict)` + `)`)).to.deep.equal(makeOk(true));
-        //expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (dict? '(2  4))` + `)`)).to.deep.equal(makeOk(false));
+        expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (dict? dict)` + `)`)).to.deep.equal(makeOk(true));
+        expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (dict? '(2  4))` + `)`)).to.deep.equal(makeOk(false));
         // expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (result->val (get (result->val (put dict 3 4)) 3))` + `)`)).to.deep.equal(makeOk(4));
         // expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (result->val (get (result->val (put (result->val (put dict 3 4) ) 3 5)) 3))` + `)`)).to.deep.equal(makeOk(5));
         // expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (result->val (get (result->val (put dict 3 4)) 4))` + `)`)).to.deep.equal(makeOk("Key not found"));
         // expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (result->val (put '(1 2) 3 4))` + `)`)).to.deep.equal(makeOk("Error: not a dictionary"));
-        // expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (result->val (get '(1 2) 1))` + `)`)).to.deep.equal(makeOk("Error: not a dictionary"));
+        expect(evalP(`(L3` + q2 + q2_test_string + `(define dict (make-dict)) (result->val (get '(1 2) 1))` + `)`)).to.deep.equal(makeOk("Error: not a dictionary"));
     });
 
     // /**
