17adb6cd03268a837e862414be551a996d7508c5
tamhrn <tamhrn@gmail.com>
2022-04-27 23:36:18 +0300

    q4 finished!

--- src/q4.ts
+++ src/q4.ts
@@ -1,7 +1,8 @@
 import { map } from 'ramda';
-import { Exp, Program, isProgram, isBoolExp, isNumExp, isStrExp, isLitExp, isVarRef, isProcExp, isIfExp, isAppExp, isPrimOp, isDefineExp, isVarDecl, isBinding, isLetExp, PrimOp, CExp } from '../imp/L3-ast';
-import { isSymbolSExp } from '../imp/L3-value';
+import { Exp, Program, isProgram, isBoolExp, isNumExp, isStrExp, isLitExp, isVarRef, isProcExp, isIfExp, isAppExp, isPrimOp, isDefineExp, isVarDecl, isBinding, isLetExp, PrimOp, CExp, LetExp, isCompoundExp } from '../imp/L3-ast';
+import { compoundSExpToString, isSymbolSExp } from '../imp/L3-value';
 import { Result, makeFailure, bind, makeOk, mapResult, safe2 } from '../shared/result';
+
 
 
 /*
@@ -10,31 +11,30 @@ Signature: l30ToJS(l2AST)
 Type: [EXP | Program] => Result<string>
 */
 export const l30ToJS = (exp: Exp | Program): Result<string>  => 
-    isProgram(exp) ? mapResult(l30ToJS, exp.exps):
+    isProgram(exp) ? bind(mapResult(l30ToJS, exp.exps), exps => makeOk(exps.join(";\n"))) :
     isBoolExp(exp) ? makeOk(exp.val? "true" : "false"):
-    isNumExp(exp) ? makeOk(exp.val.toString):
-    isStrExp(exp) ? makeOk(exp.val) :
+    isNumExp(exp) ? makeOk(exp.val.toString()):
+    isStrExp(exp) ? makeOk(`\"${exp.val}\"`) : //check
     isVarRef(exp) ? makeOk(exp.var) :
     isPrimOp(exp) ? makeOk(primOpToStr(exp.op)) :
-    isProcExp(exp) ? bind(l30ToJS(exp.body[0]), body => makeOk("( (" + exp.args.join(", ") + " => (" + body + ") )")) :
-    isIfExp(exp) ? safe2((test,althen) => makeOk("((" + test + ")" + althen + ")"))((l30ToJS(exp.test)), safe2((alt, then) => makeOk("?" + alt + " : " + then))(l30ToJS(exp.alt), (l30ToJS(exp.then)))) :
-    isDefineExp(exp) ? bind(l30ToJS(exp.val), vl => makeOk("const " + exp.var + " = " + vl)):
+    isProcExp(exp) ? bind(l30ToJS(exp.body[0]), body => makeOk("((" + map((b) => b.var, exp.args).join(",") + ") => " + body + ")")) :
+    isIfExp(exp) ? safe2((test,althen) => makeOk("(" + test + althen + ")"))((l30ToJS(exp.test)), safe2((then, alt) => makeOk(" ? " + then + " : " + alt))(l30ToJS(exp.then), (l30ToJS(exp.alt)))) :
+    isLetExp(exp) ? letToJS(exp) :
+    isDefineExp(exp) ? bind(l30ToJS(exp.val), vl => makeOk("const " + exp.var.var + " = " + vl)):
     isAppExp(exp) ? isPrimOp(exp.rator)? primOpToJs(exp.rator, exp.rands): safe2((rator: string, rands: string[]) => 
-        makeOk(rator + "(" + rands.join(", ") + ")"))(l30ToJS(exp.rator), mapResult(l30ToJS, exp.rands)):
+         makeOk(rator + "(" + rands.join(",") + ")"))(l30ToJS(exp.rator), mapResult(l30ToJS, exp.rands)):
     isLitExp(exp) ? isSymbolSExp(exp.val)? makeOk(`Symbol.for("${exp.val.val}")`):
-         makeFailure("Never") : //check
-    isLetExp(exp) ?    :
-    exp;
+          makeFailure("Never") : //check
+    makeFailure("never");
 
+const letToJS = (exp: LetExp) : Result<string> => {
+    const vars = map((b) => b.var.var, exp.bindings);
+    const vals = map((b) => b.val, exp.bindings);
 
+    return safe2((varbody: string, vals: string[]) => makeOk(varbody + "(" + vals.join(",") + ")"))
+    (bind(l30ToJS(exp.body[0]), (body:string) => makeOk("((" + vars.join(",") + ") => " + body + ")")),mapResult(l30ToJS, vals))    
+}
 
-
-    // safe2((varbody: string, vals: string[]) => makeOk(varbody + "(" + vals.join(", ") + ")"))
-    // ((safe2((vrs: string[], body: string) => makeOk("( (" + vrs.join(", ") + ")" + "=>" + "(" + body + ")")))
-    // (mapResult(l30ToJS, map((b) => b.var, exp.bindings)), l30ToJS(exp.body)), mapResult(l30ToJS, map((b) => b.val, exp.bindings)))
-
-    // safe2((vrs: string[], body: string) => makeOk("( (" + vrs.join(", ") + ")" + "=>" + "(" + body + ")"))
-    // (mapResult(l30ToJS, map((b) => b.var, exp.bindings)), l30ToJS(exp.body))
 
 const primOpToStr = (op : string) : string =>
     op === "+" ? op :
@@ -44,42 +44,25 @@ const primOpToStr = (op : string) : string =>
     op === "<" ? op:
     op === ">" ? op:
     op === "=" ? "===":
-    op === "numer?" ? "( (x) => (typeof (x) === number))":
-    op === "boolean?" ? "( (x) => (typeof (x) === boolean))":
+    op === "numer?" ? "((x) => (typeof (x) === number))":
+    op === "boolean?" ? "((x) => (typeof (x) === boolean))":
     op === "eq?" ? "===":
     op === "and" ? "&&":
     op === "or" ? "||":
-    op === "not" ? "not":
-    op === "symbol?" ? "( (x) => (typeof (x) === symbol))":
-    op === "string?" ? "( (x) => (typeof (x) === string))":
+    op === "not" ? "!":
+    op === "symbol?" ? "((x) => (typeof (x) === symbol))":
+    op === "string?" ? "((x) => (typeof (x) === string))":
     op === "string=?" ? "===":
     op;
+
 
 const primOpToJs = (rator: PrimOp, rands: CExp[]) : Result<string> =>
     rator.op === "number?" || rator.op === "boolean?" || rator.op === "symbol?" 
     || rator.op === "string?" ? bind(l30ToJS(rands[0]), (rand: string) => makeOk(primOpToStr(rator.op)+"("+rand+")")) :
-    rator.op === "not" ? bind(l30ToJS(rands[0]), (rand: string) => makeOk("(not " + rand + ")")):
+    rator.op === "not" ? bind(l30ToJS(rands[0]), (rand: string) => makeOk("(!" + rand + ")")):
     rator.op === "or" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk(rands.join("||"))):
     rator.op === "and" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk(rands.join("&&"))):
-    rator.op === "eq?" || rator.op === "string=?" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk("(" + rands.join("===") + ") ?")):
+    rator.op === "eq?" || rator.op === "string=?" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk("(" + rands.join(" === ") + ")")):
     bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk("(" + rands.join(" " + primOpToStr(rator.op) + " ") + ")"));
 
-
-
-    // isProgram(exp) ?  mapResult(l30ToJS, exp.exps):
-    // isBoolExp(exp) ? makeOk(exp.val? "true" : "false") :
-    // isNumExp(exp) ?  makeOk("( " + exp.val.toString + " )"):
-    // isStrExp(exp) ? makeOk(exp.val) :
-    // isLitExp(exp) ?  makeOk("a"):
-    // isVarRef(exp) ? makeOk(exp.var) :
-    // isVarDecl(exp) ? makeOk("a") :
-    // isProcExp(exp) ? bind(l30ToJS(exp.body[0]), body => makeOk("( (" + exp.args.join(", ") + " => (" + body + ") )")) :
-    // isIfExp(exp) ? safe2((test,althen) => makeOk("((" + test + ")" + althen + ")"))((l30ToJS(exp.test)), safe2((alt, then) => makeOk("?" + alt + " : " + then))(l30ToJS(exp.alt), (l30ToJS(exp.then)))) :
-    // isDefineExp(exp) ? safe2((vr, vl) => makeOk("const " + vr + " = " + vl))((l30ToJS(exp.var)), (l30ToJS(exp.val))) :
-    // isBinding(exp) ? safe2((a,b) => makeOk("(" + a + ")" + "(" + b + ")"))(l30ToJS(exp.var), l30ToJS(exp.val)) :
-    // isBinding(exp) ? makeOk("a"):
-    // isAppExp(exp) ?  makeOk("a"):
-    // isPrimOp(exp) ?  makeOk("a"):
-    // isLetExp(exp) ?  makeOk("a"):    
-    // exp;
 
--- test/q4.test.ts
+++ test/q4.test.ts
@@ -34,17 +34,13 @@ describe('Q4 Tests', () => {
         expect(l30toJSResult(`(f 3 4)`)).to.deep.equal(makeOk(`f(3,4)`));
     });
 
-    it("parses functions with multiple body expressions", () => {
-        expect(l30toJSResult(`(define g (lambda (x y) (+ x 2) (- y 3) (* x y)))`)).to.deep.equal(makeOk(`const g = ((x,y) => {(x + 2); (y - 3); return (x * y);})`));
-    });
-
     it("let expressions", () => {
         expect(l30toJSResult(`(let ((a 1) (b 2)) (+ a b))`)).to.deep.equal(makeOk(`((a,b) => (a + b))(1,2)`));
     });
 
 
     it('parses programs', () => {
-        expect(bind(parseL3(`(L3 (define b (> 3 4)) (define x 5) (define f (lambda (y) (+ x y))) (define g (lambda (y) (* x y))) (if (not b) (f 3) (g 4)) ((lambda (x) (* x x)) 7))`), l30ToJS)).to.deep.equal(makeOk(`const b = (3 > 4);\nconst x = 5;\nconst f = ((y) => (x + y));\nconst g = ((y) => (x * y));\n((!b) ? f(3) : g(4));\nconsole.log(((x) => (x * x))(7));`));
+        expect(bind(parseL3(`(L3 (define b (> 3 4)) (define x 5) (define f (lambda (y) (+ x y))) (define g (lambda (y) (* x y))) (if (not b) (f 3) (g 4)) ((lambda (x) (* x x)) 7))`), l30ToJS)).to.deep.equal(makeOk(`const b = (3 > 4);\nconst x = 5;\nconst f = ((y) => (x + y));\nconst g = ((y) => (x * y));\n((!b) ? f(3) : g(4));\n((x) => (x * x))(7)`));
     });
 
     it("literal expressions", () => {
