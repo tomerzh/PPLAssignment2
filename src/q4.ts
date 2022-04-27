e3c18ba76973260f9dfd8aa3ccd77956c4fc76f7
tamhrn <tamhrn@gmail.com>
2022-04-27 07:14:31 +0300

    q4almostDone

--- src/q4.ts
+++ src/q4.ts
@@ -1,6 +1,8 @@
 import { map } from 'ramda';
-import { Exp, Program, isProgram, isBoolExp, isNumExp, isStrExp, isLitExp, isVarRef, isProcExp, isIfExp, isAppExp, isPrimOp, isDefineExp, isVarDecl, isBinding, isLetExp } from '../imp/L3-ast';
+import { Exp, Program, isProgram, isBoolExp, isNumExp, isStrExp, isLitExp, isVarRef, isProcExp, isIfExp, isAppExp, isPrimOp, isDefineExp, isVarDecl, isBinding, isLetExp, PrimOp, CExp } from '../imp/L3-ast';
+import { isSymbolSExp } from '../imp/L3-value';
 import { Result, makeFailure, bind, makeOk, mapResult, safe2 } from '../shared/result';
+
 
 /*
 Purpose: Transform L3 AST to JavaScript program string
@@ -8,63 +10,76 @@ Signature: l30ToJS(l2AST)
 Type: [EXP | Program] => Result<string>
 */
 export const l30ToJS = (exp: Exp | Program): Result<string>  => 
-    isProgram(exp) ?  mapResult(l30ToJS, exp.exps):
-    isBoolExp(exp) ? makeOk(exp.val? "true" : "false") :
-    isNumExp(exp) ?  makeOk("( " + exp.val.toString + " )"):
+    isProgram(exp) ? mapResult(l30ToJS, exp.exps):
+    isBoolExp(exp) ? makeOk(exp.val? "true" : "false"):
+    isNumExp(exp) ? makeOk(exp.val.toString):
     isStrExp(exp) ? makeOk(exp.val) :
-    //isLitExp(exp) ?  makeOk():
     isVarRef(exp) ? makeOk(exp.var) :
-    //isVarDecl(exp) ?  :
+    isPrimOp(exp) ? makeOk(primOpToStr(exp.op)) :
     isProcExp(exp) ? bind(l30ToJS(exp.body[0]), body => makeOk("( (" + exp.args.join(", ") + " => (" + body + ") )")) :
-    isIfExp(exp) ?  :
-    isBinding(exp) ? :
-    isAppExp(exp) ?  :
-    isPrimOp(exp) ?  :
-    isLetExp(exp) ?  :
-    isDefineExp(exp) ?  :
+    isIfExp(exp) ? safe2((test,althen) => makeOk("((" + test + ")" + althen + ")"))((l30ToJS(exp.test)), safe2((alt, then) => makeOk("?" + alt + " : " + then))(l30ToJS(exp.alt), (l30ToJS(exp.then)))) :
+    isDefineExp(exp) ? bind(l30ToJS(exp.val), vl => makeOk("const " + exp.var + " = " + vl)):
+    isAppExp(exp) ? isPrimOp(exp.rator)? primOpToJs(exp.rator, exp.rands): safe2((rator: string, rands: string[]) => 
+        makeOk(rator + "(" + rands.join(", ") + ")"))(l30ToJS(exp.rator), mapResult(l30ToJS, exp.rands)):
+    isLitExp(exp) ? isSymbolSExp(exp.val)? makeOk(`Symbol.for("${exp.val.val}")`):
+         makeFailure("Never") : //check
+    isLetExp(exp) ?    :
     exp;
 
-const lambdaToStr = (exp: Exp): string => 
-        
 
 
 
+    // safe2((varbody: string, vals: string[]) => makeOk(varbody + "(" + vals.join(", ") + ")"))
+    // ((safe2((vrs: string[], body: string) => makeOk("( (" + vrs.join(", ") + ")" + "=>" + "(" + body + ")")))
+    // (mapResult(l30ToJS, map((b) => b.var, exp.bindings)), l30ToJS(exp.body)), mapResult(l30ToJS, map((b) => b.val, exp.bindings)))
+
+    // safe2((vrs: string[], body: string) => makeOk("( (" + vrs.join(", ") + ")" + "=>" + "(" + body + ")"))
+    // (mapResult(l30ToJS, map((b) => b.var, exp.bindings)), l30ToJS(exp.body))
+
+const primOpToStr = (op : string) : string =>
+    op === "+" ? op :
+    op === "-" ? op :
+    op === "*" ? op :
+    op === "/" ? op:
+    op === "<" ? op:
+    op === ">" ? op:
+    op === "=" ? "===":
+    op === "numer?" ? "( (x) => (typeof (x) === number))":
+    op === "boolean?" ? "( (x) => (typeof (x) === boolean))":
+    op === "eq?" ? "===":
+    op === "and" ? "&&":
+    op === "or" ? "||":
+    op === "not" ? "not":
+    op === "symbol?" ? "( (x) => (typeof (x) === symbol))":
+    op === "string?" ? "( (x) => (typeof (x) === string))":
+    op === "string=?" ? "===":
+    op;
+
+const primOpToJs = (rator: PrimOp, rands: CExp[]) : Result<string> =>
+    rator.op === "number?" || rator.op === "boolean?" || rator.op === "symbol?" 
+    || rator.op === "string?" ? bind(l30ToJS(rands[0]), (rand: string) => makeOk(primOpToStr(rator.op)+"("+rand+")")) :
+    rator.op === "not" ? bind(l30ToJS(rands[0]), (rand: string) => makeOk("(not " + rand + ")")):
+    rator.op === "or" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk(rands.join("||"))):
+    rator.op === "and" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk(rands.join("&&"))):
+    rator.op === "eq?" || rator.op === "string=?" ? bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk("(" + rands.join("===") + ") ?")):
+    bind(mapResult(l30ToJS, rands), (rands: string[]) => makeOk("(" + rands.join(" " + primOpToStr(rator.op) + " ") + ")"));
 
 
 
+    // isProgram(exp) ?  mapResult(l30ToJS, exp.exps):
+    // isBoolExp(exp) ? makeOk(exp.val? "true" : "false") :
+    // isNumExp(exp) ?  makeOk("( " + exp.val.toString + " )"):
+    // isStrExp(exp) ? makeOk(exp.val) :
+    // isLitExp(exp) ?  makeOk("a"):
+    // isVarRef(exp) ? makeOk(exp.var) :
+    // isVarDecl(exp) ? makeOk("a") :
+    // isProcExp(exp) ? bind(l30ToJS(exp.body[0]), body => makeOk("( (" + exp.args.join(", ") + " => (" + body + ") )")) :
+    // isIfExp(exp) ? safe2((test,althen) => makeOk("((" + test + ")" + althen + ")"))((l30ToJS(exp.test)), safe2((alt, then) => makeOk("?" + alt + " : " + then))(l30ToJS(exp.alt), (l30ToJS(exp.then)))) :
+    // isDefineExp(exp) ? safe2((vr, vl) => makeOk("const " + vr + " = " + vl))((l30ToJS(exp.var)), (l30ToJS(exp.val))) :
+    // isBinding(exp) ? safe2((a,b) => makeOk("(" + a + ")" + "(" + b + ")"))(l30ToJS(exp.var), l30ToJS(exp.val)) :
+    // isBinding(exp) ? makeOk("a"):
+    // isAppExp(exp) ?  makeOk("a"):
+    // isPrimOp(exp) ?  makeOk("a"):
+    // isLetExp(exp) ?  makeOk("a"):    
+    // exp;
 
-
-
-
-
-
-// Add a quote for symbols, empty and compound sexp - strings and numbers are not quoted.
-const unparseLitExp = (le: LitExp): string =>
-    isEmptySExp(le.val) ? `'()` :
-    isSymbolSExp(le.val) ? `'${valueToString(le.val)}` :
-    isCompoundSExp(le.val) ? `'${valueToString(le.val)}` :
-    `${le.val}`;
-
-const unparseLExps = (les: Exp[]): string =>
-    map(unparseL3, les).join(" ");
-
-const unparseProcExp = (pe: ProcExp): string => 
-    `(lambda (${map((p: VarDecl) => p.var, pe.args).join(" ")}) ${unparseLExps(pe.body)})`
-
-const unparseLetExp = (le: LetExp) : string => 
-    `(let (${map((b: Binding) => `(${b.var.var} ${unparseL3(b.val)})`, le.bindings).join(" ")}) ${unparseLExps(le.body)})`
-
-export const unparseL3 = (exp: Program | Exp): string =>
-    isBoolExp(exp) ? valueToString(exp.val) :
-    isNumExp(exp) ? valueToString(exp.val) :
-    isStrExp(exp) ? valueToString(exp.val) :
-    isLitExp(exp) ? unparseLitExp(exp) :
-    isVarRef(exp) ? exp.var :
-    isProcExp(exp) ? unparseProcExp(exp) :
-    isIfExp(exp) ? `(if ${unparseL3(exp.test)} ${unparseL3(exp.then)} ${unparseL3(exp.alt)})` :
-    isAppExp(exp) ? `(${unparseL3(exp.rator)} ${unparseLExps(exp.rands)})` :
-    isPrimOp(exp) ? exp.op :
-    isLetExp(exp) ? unparseLetExp(exp) :
-    isDefineExp(exp) ? `(define ${exp.var.var} ${unparseL3(exp.val)})` :
-    isProgram(exp) ? `(L3 ${unparseLExps(exp.exps)})` :
-    exp;    
